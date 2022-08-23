e726aa75d4c0ad7609d3d1b01c71d0d83f1cce7c
Dervoo <kojar1997@gmail.com>
2022-08-23 19:00:14 +0200

    format files

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -7,19 +7,28 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Nft is Ownable, ERC721URIStorage {
-  using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor(string memory _name, string memory _symbol, address receiver, string[] memory uris) public ERC721(_name, _symbol) {
-    for (uint256 i = 0; i < uris.length; i++) {
-      mint(receiver, uris[i]);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address receiver,
+        string[] memory uris
+    ) public ERC721(_name, _symbol) {
+        for (uint256 i = 0; i < uris.length; i++) {
+            mint(receiver, uris[i]);
+        }
     }
-    }
-    function mint(address receiver, string memory tokenURI) public returns (uint256) {
-        _tokenIds.increment(); 
-         uint256 newItemId = _tokenIds.current();
-    _mint(receiver, newItemId);
-    _setTokenURI(newItemId, tokenURI);
-    return newItemId;
+
+    function mint(address receiver, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(receiver, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
     }
 }
